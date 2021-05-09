0ef18ef84bf8d41fed3f8d044586d945a5cc45eb
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-10 03:08:53 +0500

    Created database with room DAO

--- app/src/main/java/uz/unzosoft/likerun/db/Run.kt
+++ app/src/main/java/uz/unzosoft/likerun/db/Run.kt
@@ -1,7 +1,8 @@
-package uz.unzosoft.likerun.ui
+package uz.unzosoft.likerun.db
 
 import android.graphics.Bitmap
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.sql.Timestamp
 
 @Entity(tableName = "running_table")
@@ -10,5 +11,11 @@ data class Run(
     var timestamp: Long = 0L,
     var avgSpeedInKHM: Float = 0f,
     var distanceInMeters: Int = 0,
-    var timeInMillis: Long = 0L
-)
+    var timeInMillis: Long = 0L,
+    var caloriesBurned: Int = 0
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+
+
+}
