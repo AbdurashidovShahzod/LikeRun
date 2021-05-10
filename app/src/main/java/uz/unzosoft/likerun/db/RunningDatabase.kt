94966ba92e4814b00d73bd522c3038892bd1e587
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-10 05:23:29 +0500

    Created database with room Type converter

---
+++ app/src/main/java/uz/unzosoft/likerun/db/RunningDatabase.kt
@@ -0,0 +1,17 @@
+package uz.unzosoft.likerun.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+    abstract fun getRunDAO(): RunDAO
+
+}
