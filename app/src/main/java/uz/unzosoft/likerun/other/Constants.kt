535b8378b343aacc4fee88d45a1f9e6896b53064
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-11 01:51:12 +0500

    Google map Tracking service

--- app/src/main/java/uz/unzosoft/likerun/other/Constants.kt
+++ app/src/main/java/uz/unzosoft/likerun/other/Constants.kt
@@ -11,6 +11,9 @@ object Constants {
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    const val NOTIFICATION_ID = 1
 
     fun statusBar(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
--- app/src/main/java/uz/unzosoft/likerun/services/TrackingService.kt
+++ app/src/main/java/uz/unzosoft/likerun/services/TrackingService.kt
@@ -1,11 +1,22 @@
 package uz.unzosoft.likerun.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import timber.log.Timber
+import uz.unzosoft.likerun.R
 import uz.unzosoft.likerun.other.Constants.ACTION_PAUSE_SERVICE
 import uz.unzosoft.likerun.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import uz.unzosoft.likerun.other.Constants.ACTION_STOP_SERVICE
+import uz.unzosoft.likerun.other.Constants.NOTIFICATION_CHANNEL_ID
+import uz.unzosoft.likerun.other.Constants.NOTIFICATION_CHANNEL_NAME
+import uz.unzosoft.likerun.other.Constants.NOTIFICATION_ID
 
 class TrackingService : LifecycleService() {
 
@@ -19,5 +30,34 @@ class TrackingService : LifecycleService() {
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun foregroundService() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+
+        val notificationBuilder = NotificationCompat
+            .Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("LikeRun")
+            .setContentText("00:00:00")
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel =
+            NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_CHANNEL_NAME,
+                IMPORTANCE_LOW
+            )
+        notificationManager.createNotificationChannel(channel)
     }
 }
