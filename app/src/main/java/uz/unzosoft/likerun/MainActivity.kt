f44e89291191c3e6b3332e7c8df42730db6590b1
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-10 06:50:32 +0500

    Dependency injection dagger hilt

--- app/src/main/java/uz/unzosoft/likerun/MainActivity.kt
+++ app/src/main/java/uz/unzosoft/likerun/MainActivity.kt
@@ -3,17 +3,25 @@ package uz.unzosoft.likerun
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
 import uz.unzosoft.likerun.databinding.ActivityMainBinding
+import uz.unzosoft.likerun.db.RunDAO
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var dao: RunDAO
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         transparentStatusBar()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.rootView)
-
+        Log.d("daoRun", "${dao.hashCode()}")
 
     }
 
--- app/src/main/java/uz/unzosoft/likerun/di/AppModule.kt
+++ app/src/main/java/uz/unzosoft/likerun/di/AppModule.kt
@@ -4,18 +4,31 @@ import android.content.Context
 import android.provider.DocumentsContract
 import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import uz.unzosoft.likerun.db.RunningDatabase
+import uz.unzosoft.likerun.other.Constants
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
     fun provideRunningDatabase(@ApplicationContext app: Context) = Room.databaseBuilder(
         app,
         RunningDatabase::class.java,
-        RUNNING_DATABASE_NAME
-    )
+        Constants.RUNNING_DATABASE_NAME
+    ).build()
+
+
+    @Provides
+    @Singleton
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+
+
 }
